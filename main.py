1d49c93386c727339828c25c3f0f9278640f9c7d
Gagandeep Singh <singh.gagandeep1103@gmail.com>
2024-03-31 18:30:09 -0400

    all

---
+++ main.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "073bcfbc",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import json\n",
+    "import mlflow\n",
+    "import os\n",
+    "import tempfile\n",
+    "import wandb\n",
+    "import hydra \n",
+    "from omegaconf import DictConfig"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "206acc8d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "steps = []"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8f9ff339",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.18"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
